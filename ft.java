01f7b4aa503f1335473ac7a6d92ea89633d5d7bc
glinkybeen <92579228+glinkybeen@users.noreply.github.com>
2024-09-27 13:13:33 +0500

    Update ft.java

--- ft.java
+++ ft.java
@@ -4,39 +4,57 @@ public class ft {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Приветствую, вы можете выполнить математическую операцию с любыми двумя числами");
-        System.out.print("Введите первое число: ");
-        double num1 = scanner.nextDouble();
+
+        System.out.println("Приветствую!");
 
         System.out.print("Введите оператор (+, -, *, /): ");
         char operator = scanner.next().charAt(0);
 
-        System.out.print("Введите второе число: ");
-        double num2 = scanner.nextDouble();
+        System.out.println("Введите числа (вводите по одному, для завершения введите 'exit'):");
 
-        double result;
+        double result = 0;
+        boolean firstInput = true;
 
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
+        while (true) {
+            String input = scanner.next();
+
+            if (input.equalsIgnoreCase("exit")) {
                 break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Ошибка: деление на ноль!");
-                    return;
+            }
+            double num;
+            try {
+                num = Double.parseDouble(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка: введите число или 'exit' для завершения!");
+                continue;
+            }
+            if (firstInput) {
+                result = num;
+                firstInput = false;
+            } else {
+                switch (operator) {
+                    case '+':
+                        result += num;
+                        break;
+                    case '-':
+                        result -= num;
+                        break;
+                    case '*':
+                        result *= num;
+                        break;
+                    case '/':
+                        if (num != 0) {
+                            result /= num;
+                        } else {
+                            System.out.println("Ошибка: деление на ноль!");
+                            return;
+                        }
+                        break;
+                    default:
+                        System.out.println("Ошибка: неверный оператор!");
+                        return;
                 }
-                break;
-            default:
-                System.out.println("Ошибка: неверный оператор!");
-                return;
+            }
         }
         System.out.println("Результат: " + result);
     }
